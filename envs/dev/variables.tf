93ed3d72048737693cf08fe14c90a58bcb524323
Nirmal Sona <126976548+nirmalsona@users.noreply.github.com>
2026-07-20 23:25:51 +0200

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,9 +1,6 @@
-variable "rgs" {}
-variable "vnets" {}
-variable "subnets" {}
-variable "pips" {}
-
-variable "nics_vim" {}
-
-
+variable "rgs" { type = list(string) }     # Multiple RG names ke liye
+variable "vnets" { type = list(string) }   # Multiple VNet names ke liye
+variable "subnets" { type = list(string) } # Multiple Subnet CIDR blocks ke liye
+variable "pips" { type = list(string) }    # Multiple Public IP names ke liye
+variable "nics_vim" { type = list(string) }# Multiple NIC names ke liye
 
