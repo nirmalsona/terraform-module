b3ac12d6bcf8319f22fa97adba899b2a350bcd04
Nirmal Sona <126976548+nirmalsona@users.noreply.github.com>
2026-08-08 03:11:58 +0530

    Modify VM image reference and SKU
    Updated the source image reference for the virtual machine to Ubuntu 24.04 and adjusted the SKU accordingly.

--- modules/5.azurerm_network_interface_card & vm/main.tf
+++ modules/5.azurerm_network_interface_card & vm/main.tf
@@ -48,11 +48,11 @@ resource "azurerm_linux_virtual_machine" "vms" {
   }
 
  source_image_reference {
-  publisher = "Canonical"
-  offer     = "0001-com-ubuntu-server-jammy"
-  sku       = "22_04-lts"
-  version   = "latest"
-}
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
   }
 
 
